3dc78991b9e3d1ca10704b637c826becf2958526
Kiran Namawar <kirannamawar@gmail.com>
2025-03-25 15:22:37 +0530

    initial commit: set up project structure with Hono

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/:code', async (c) => {
+  const code = c.req.param('code');
+  const targetUrl = `https://tamatar.store/api/tmtr/${code}`;
+
+  return c.redirect(targetUrl, 307);
+})
+
+
+export default app;
