8f3eb4922799bf4379820248fdce04dd4c41a8ed
Kiran Namawar <kirannamawar@gmail.com>
2025-03-25 15:25:10 +0530

    testing

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ const app = new Hono()
 app.get('/:code', async (c) => {
   const code = c.req.param('code');
   const targetUrl = `https://tamatar.store/api/tmtr/${code}`;
-
   return c.redirect(targetUrl, 307);
 })
 
