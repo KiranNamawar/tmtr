046ea8b67c5328079e5099a0d701a38f79ec5003
Kiran Namawar <kirannamawar@gmail.com>
2025-05-05 10:28:15 +0530

    - Enhanced routing logic to handle additional path segments for redirecting based on the `code` parameter.

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,20 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/:code', async (c) => {
+app.get('/:code/*', async (c) => {
   const code = c.req.param('code');
-  const targetUrl = `https://tamatar.store/api/tmtr/${code}`;
-  return c.redirect(targetUrl, 307);
-})
+  const restPath = c.req.param('*') || '';
+  let targetUrl = '';
 
+  if (code === 'me') {
+    targetUrl = `https://tamatar.store/me/${restPath}`;
+  } else if (code === 'dev') {
+    targetUrl = `https://tamatar.store/dev/${restPath}`;
+  } else {
+    targetUrl = `https://tamatar.store/xyz/${code}/${restPath}`;
+  }
+
+  return c.redirect(targetUrl, 307);
+});
 
 export default app;
